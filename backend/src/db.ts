cdc339f0941c360290ebcfb8c4866b08d8208891
Ishant <ishant@almabase.com>
2021-04-26 11:40:55 +0530

    environment variables set

--- backend/src/db.ts
+++ backend/src/db.ts
@@ -6,12 +6,12 @@ const isProductionEnvironment = process.env.NODE_ENV === "production";
 export default async ()=>{
   createConnection({
     type: "postgres",
-    database:  "pipfi",
+    database: process.env.DATABASE ||  "pipfi",
     entities: [join(__dirname, "./entities/*.*")],
     username: process.env.USERNAME || 'postgres',
     password: process.env.PASSWORD || 'postgres',
     port: 5432,
-    host: 'postgres',
+    host: process.env.HOST ||  'postgres',
     logging: !isProductionEnvironment,
     synchronize: !isProductionEnvironment,
   })
